ab8328686d88e4e9cd0fabd3ead88a73ee791782
tdurnford <tjdford@gmail.com>
2016-09-23 13:18:01 -0700

    Initial commit

---
+++ hw1.py
@@ -0,0 +1,288 @@
+from __future__ import print_function
+import csv
+
+
+def summary_statistics(table, keyList):
+    
+    print('Summary Stats:')
+    print("No. of instances: " + str(len(table)))
+    print("Duplicates: " + str(find_duplicates(table, keyList)))
+    
+
+    stats = [['============',  '=====',  '=====',  '=======',  '======',  '====='],
+            ['attribute','min', 'max', 'mid', 'avg', 'med'],
+            ['============',  '=====',  '=====',  '=======',  '======',  '====='],
+            ['mpg', minimum(table,0), maximum(table,0), mid(table,0), round(average(table,0),-1), median(table,0)],
+            ['displacement', minimum(table,2), maximum(table,2), mid(table,2), round(average(table,2),-1), median(table,2)],
+            ['horsepower', minimum(table,3), maximum(table,3), mid(table,3), round(average(table,3),-1), median(table,3)],
+            ['weight', minimum(table,4), maximum(table,4), mid(table,4), round(average(table,4),-1), median(table,4)],
+            ['acceleration', minimum(table,5), maximum(table,5), mid(table,5), round(average(table,5),-1), median(table,5)],
+            ['MSRP', minimum(table,9), maximum(table,9), mid(table,9), round(average(table,9),-1), median(table,9)],
+            ['============',  '=====',  '=====',  '=======',  '======',  '=====']]
+
+    cols = zip(*stats)
+    col_widths = [ max(len(str(value))+2 for value in col) for col in cols ]
+    format = ''.join(['%%%ds' % width for width in col_widths ])
+    for row in stats:
+        print(format % tuple(row))
+    
+        
+
+def run(filename, keyList):
+    
+    table = read_csv(filename)
+    
+    
+    print(filename)
+    print_line()
+    print("No. of instances: " + str(len(table)))
+    print("Duplicates: " + str(find_duplicates(table, keyList)))
+    print()
+    print_line()
+    
+    
+def read_csv(filename):
+    
+    the_file = open(filename, 'r')
+    the_reader = csv.reader(the_file, dialect = 'excel')
+    table = []
+    
+    for row in the_reader:
+        if len(row) > 0:
+            table.append(row)
+    the_file.close()
+    
+    return table
+
+
+def create_dictionary(filename, keyList):
+    with open(filename, mode='r') as infile:
+        reader = csv.reader(infile)
+        dictionary = {create_key(rows,keyList):rows for rows in reader}
+    return dictionary
+
+
+def create_key(row, keyList):
+    if len(keyList) == 1:
+        return row[keyList[0]]
+    else:
+        return str(row[keyList[0]]) + create_key(row,keyList[1:])
+
+
+def combine_tables(file1, keyList1, file2, keyList2, newFile):
+    
+    table1 = create_dictionary(file1, keyList1)
+    table2 = create_dictionary(file2, keyList2)
+                
+    newTable = []
+
+    for key in table1:
+        if key in table2:
+
+            newTable.append(table2[key]+[table1[key][2]])
+            table2.pop(key, None)
+        else:
+            line = table1[key]
+            
+            newTable.append(['NA','NA','NA','NA','NA','NA'] + [line[1]]+ ['NA'] + [line[0]]+ [line[2]])
+    
+    for key in table2:
+        newTable.append(table2[key]+['NA']) 
+
+    with open(newFile, mode='w') as csvfile:
+        writer = csv.writer(csvfile)
+        [writer.writerow(row) for row in newTable]
+    
+
+def find_duplicates(table, keyList):
+    
+    duplicates = []
+
+    i = 1
+    for row1 in table:
+        for row2 in table[i:]:
+            if check_keys(row1, row2, keyList):
+                duplicates.append(row1)
+                duplicates.append(row2)
+                
+        i +=1
+        
+    return duplicates
+
+
+def check_keys(row1, row2, keyList):
+
+    if len(keyList) == 1:
+        return row1[keyList[0]] == row2[keyList[0]]
+    else:
+        return row1[keyList[0]] == row2[keyList[0]] and check_keys(row1, row2, keyList[1:])
+
+
+def remove_incomplete_rows(table, indexList):
+
+    newTable = []
+
+    for row in table:
+        if check_NA(row, indexList):
+            newTable.append(row)
+            
+    summary_statistics(newTable, [8,6])
+    return newTable
+
+
+def replace_data(table, indexList, key):
+
+    newTable = []
+
+    for row in table:
+        if not check_NA(row, indexList):
+            subset = get_subset(table, key, row[key])
+            for index in indexList:
+                if row[index] == 'NA':
+                    row[index] = average(subset,index)
+        newTable.append(row)
+
+                    
+    summary_statistics(newTable, [8,6])
+
+
+def get_subset(table, keyIndex, keyValue):
+
+    subset = []
+
+    for row in table:
+        if row[keyIndex] == keyValue:
+            subset.append(row)
+
+    return subset
+
+
+def check_NA(row, indexList):
+
+    if len(indexList) == 1:
+        return row[indexList[0]] != 'NA'
+    else:
+        return row[indexList[0]] != 'NA'and check_NA(row, indexList[1:])
+
+
+def get_column(table, index):
+
+    values = []
+
+    for rows in table:
+        if 'NA' != rows[index]:
+            values.append(rows[index])
+
+    return values
+
+
+def get_column_as_floats(table, index):
+
+    vals = []
+
+    for rows in table:
+        if rows[index] != 'NA':
+            vals.append(float(rows[index]))
+    return vals
+
+
+def minimum(table, index):
+    return min(get_column_as_floats(table,index))
+
+
+def maximum(table, index):
+    return max(get_column_as_floats(table,index))
+
+
+def mid(table, index):
+    column = get_column_as_floats(table, index)
+    return (max(column)+min(column))/2.0
+
+
+def average(table,index):
+    column = get_column_as_floats(table, index)
+    if len(column) == 0:
+        return 'NA'
+    else:
+        return average_column(column)
+
+
+def average_column(column):
+    return sum(column)/float(len(column))
+
+
+def median(table, index):
+    column = get_column_as_floats(table, index)
+    column.sort()
+    mid = len(column)
+    if mid%2 == 0:
+        return average_column([column[mid/2],column[(mid/2)-1]])
+    else:
+        return column[mid/2]
+
+
+def print_line():
+    print("--------------------------------------------------")
+
+
+def print_double_line():
+    print("============  =====  =====  =======  ======  =====")
+
+
+def clean(table):
+
+    clean = []
+
+    for row in table:
+        if (row[0] == 'NA' and row[9] != 'NA'):
+            clean.append(row)
+
+    clean.sort(key=lambda x:x[8])
+
+    subset = get_subset(table,8,clean[1][8])
+
+    for row in subset:
+        print(row)
+
+
+def main():
+
+    print_line()
+    run('auto-prices.txt', [0,1])
+    run('auto-mpg.txt', [8,6])
+    run('auto-prices-nodups.txt', [0,1])
+    run('auto-mpg-nodups.txt', [8,6])
+
+    combine_tables('auto-prices-nodups.txt', [0,1],'auto-mpg-nodups.txt', [8,6], 'auto-data.txt')
+    
+    run('auto-data.txt',[6,8])
+    run('auto-prices-clean.txt', [0,1])
+    run('auto-mpg-clean.txt', [8,6])
+
+    table = read_csv('auto-data.txt')
+
+    print_line()
+    print('combined table (saved as auto-data.txt):')
+    print_line()
+    summary_statistics(table, [8,6])
+
+    print_line()
+    print('combined table (rows w/ missing values removed):')
+    print_line()
+    
+    newTable = remove_incomplete_rows(table,[0,2,3,4,5,9])
+
+    print_line()
+    print('combined table (missing values replaced w/ corresponding average):')
+    print_line()
+    
+    replace_data(table, [0,2,3,4,5,9],8)
+
+    print_line()
+    print('combined table (missing values replaced by yearly average):')
+    print_line()
+    replace_data(table, [0,2,3,4,5,9],6)
+
+if __name__ == '__main__':
+    main()
+
---
+++ hw2.py
@@ -0,0 +1,181 @@
+import matplotlib
+
+matplotlib.use('pdf')
+
+import matplotlib.pyplot as pyplot
+import numpy as numpy
+
+from hw1 import read_csv, get_column, get_column_as_floats
+
+
+def scatter_plot(table, xIndex, yIndex):
+
+    ys = []
+    xs = []
+
+    for row in table:  # get x and y values from table
+        if row[xIndex] != 'NA' and row[yIndex] != 'NA':
+            ys.append(float(row[yIndex]))
+            xs.append(float(row[xIndex]))
+
+    pyplot.figure()
+
+    pyplot.xlim(int(min(xs)) * 0.95, int(max(xs) * 1.05))  # set x bounds on graph
+    pyplot.ylim(int(min(ys)) * 0.95, int(max(ys) * 1.05))  # set y bounds on graph
+
+    pyplot.xlabel('Year')  # x label
+    pyplot.ylabel('MPG')  # y label
+
+    pyplot.grid()
+
+    pyplot.scatter(xs, ys, color='g')
+
+    pyplot.savefig('fig8.pdf')  # save graph
+
+
+def pie_char(freq):
+
+    total = sum(freq[1])  # get total (probably should be length)
+    percents = []
+
+    for item in freq[1]:
+        percents.append(item / float(total))
+
+    pyplot.figure()
+    pyplot.pie(percents, labels=freq[0])
+
+    pyplot.savefig('fig3.pdf')
+
+
+def strip_char(table, index):
+
+    column = get_column_as_floats(table, index)
+    y = [1] * len(column)
+
+    pyplot.figure()
+
+    xrng = numpy.arange(len(column))
+    pyplot.xticks(xrng, column)
+
+    pyplot.gca().get_yaxis().set_visible(False)
+    pyplot.plot(column, y, marker='.', markersize=50, alpha=0.2)
+
+    pyplot.savefig('fig4.pdf')
+
+
+def box_plot(table, index):
+
+    pyplot.figure()
+
+    data = group_by(table, index)
+    xrng = numpy.arange(len(data[0]), 1)
+
+    pyplot.xticks(xrng, data[0])
+    pyplot.xlabel('Year')
+    pyplot.ylabel('MPG')
+    pyplot.boxplot(data[1])
+
+    pyplot.savefig('fig7.pdf')
+
+
+def frequency_chart(freq):
+    xs = freq[0]
+    ys = freq[1]
+
+    xrng = numpy.arange(len(xs))
+    yrng = numpy.arange(max(ys) + 2)
+
+    pyplot.bar(xrng, ys, 0.5, alpha=0.75, align='center', color='r')
+
+    pyplot.xticks(xrng, freq[0])
+    pyplot.yticks(yrng)
+
+    pyplot.grid(True)
+
+    pyplot.savefig('fig2.pdf')
+
+
+def frequency(table, index):
+    # returns frequency of values in a column
+
+    column = get_column(table, index)
+    column.sort()
+
+    cats = []
+
+    for item in column:
+        if item not in cats:
+            cats.append(item)
+
+    freq = [0]*len(cats)
+
+    for item in column:
+        for index in range(len(cats)):
+            if item == cats[index]:
+                freq[index] += 1
+
+    return cats, freq
+
+
+def group_by(table, index):
+
+    dict = {}
+
+    for row in table:
+        if row[0] != 'NA':
+            if row[index] in dict:
+                dict[row[index]].append(float(row[0]))
+            else:
+                dict.update({row[index]: [float(row[0])]})
+
+    keys = sorted(dict.keys())
+    values = []
+
+    for key in keys:
+        values.append(dict[key])
+
+    return keys, values
+
+
+def get_cutoffs(table, index, num):
+
+    col = get_column_as_floats(table, index)
+
+    max_value = int(max(col))
+    min_value = int(min(col))
+
+    width = int((max_value-min_value)/num)
+
+    return list(range(min_value + width, max_value+1, width))
+
+
+def cut_off_frequency(table, index, cutoffs):
+
+    freq = [0]*len(cutoffs)
+
+    cutoffs.sort()
+    col = get_column_as_floats(table, index)
+
+    for item in col:
+        for index in range(len(cutoffs)):
+            if item <= cutoffs[index]:
+                freq[index] += 1
+                break
+
+    return cutoffs, freq
+
+
+def main():
+
+    table = read_csv('auto-data.txt')
+
+    frequency_chart(frequency(table, 0))
+    freq = cut_off_frequency(table, 0, get_cutoffs(table, 0, 10))
+    pie_char(freq)
+
+    strip_char(table, 0)
+    box_plot(table, 6)
+    scatter_plot(table, 6, 0)
+
+
+main()
