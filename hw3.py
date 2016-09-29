9a6842c3ca581f07dbd458cd4f01a0183ffd1fdb
tdurnford <tjdford@gmail.com>
2016-09-29 14:44:43 -0700

    Minor changes

--- hw3.py
+++ hw3.py
@@ -1,15 +1,17 @@
 from hw1 import read_csv, get_column, get_column_as_floats
 from hw2 import remove_incomplete_rows
 
+
 def distance(row, instance, n):
-    
+
     return 1
 
-def normalize(row):
-    maximum = max(row)
-    minimum = min(row)
+
+def normalize(col):
+    maximum = max(col)
+    minimum = min(col)
     rng = maximum - minimum
-    return[(r-minimum)/float(maximum) for r in row]
+    return[(item - minimum)/float(rng) for item in col]
 
 
 def select_class_label(top_k_rows):
@@ -25,8 +27,7 @@ def knn_classifier(trainingSet, n, instance, k):
     distances = []
 
     for row in trainingSet:
-        d = distance(row, instance, n)
-        distances.append([d, row])
+        distances.append([distance(row, instance, n), row])
 
     distances.sort(key=lambda x: x[0])
     top_k_rows = distances[:k]
