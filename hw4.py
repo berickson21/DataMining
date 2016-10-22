552137d7bb45c388d6afa0f3777ceab8518f6ede
Blake <blake.erickson721@gmail.com>
2016-10-22 15:44:07 -0700

    Merge remote-tracking branch 'refs/remotes/origin/b4'

--- hw4.py
+++ hw4.py
@@ -32,7 +32,8 @@ def naive_bayes(table, indexes, label_index):  # step 1
     print '\tRandomSubsample(k=10, 2:1 Train / Test)'
     print '\t\taccuracy = ' + str(1) + ', error rate = ' + str(0)
     print '\tStratified 10-Fold Cross Validation'
-    print '\t\taccuracy = ' + str(stratified_folds_accuracy) + ', error rate = ' + str(1 - stratified_folds_accuracy)
+    print '\t\taccuracy = ' + str(stratified_folds_accuracy) + ', error rate = '\
+        + str(1 - stratified_folds_accuracy)
 
     print_double_line('Naive Bayes Confusion Matrix Predictive Accuracy')
 
@@ -47,7 +48,8 @@ def knn(table, indexes, label_index, k):
     print_double_line('K-nn Stratified k-Folds Predictive Accuracy')
 
     s = StratifiedFolds(table, indexes, label_index) \
-              + ' actual: ' + str(n.convert(instance[0], [13, 14, 16, 19, 23, 26, 30, 36, 44]))
+        + ' actual: ' + \
+        str(n.convert(instance[0], [13, 14, 16, 19, 23, 26, 30, 36, 44]))
 
     print_double_line('Step 1b: Predictive Accuracy')
 
@@ -60,7 +62,8 @@ def knn(table, indexes, label_index, k):
     print '\tRandomSubsample(k=10, 2:1 Train / Test)'
     print '\t\taccuracy = ' + str(1) + ', error rate = ' + str(0)
     print '\tStratified 10-Fold Cross Validation'
-    print '\t\taccuracy = ' + str(stratified_folds_accuracy) + ', error rate = ' + str(1 - stratified_folds_accuracy)
+    print '\t\taccuracy = ' + str(stratified_folds_accuracy) + ', error rate = '\
+        + str(1 - stratified_folds_accuracy)
 
     print_double_line(' K-nn Confusion Matrix Predictive Accuracy')
 
@@ -75,12 +78,12 @@ def naive_bayes_titanic(table, indexes, label_index):  # step 1
     for instance in sample(table, 5):
         print '\tinstance: ' + str(instance)
         print '\tclass: ' + str(n.classify(instance)) \
-                    + ' actual: ' + str(n.convert_yes_no(instance[3]))
+            + ' actual: ' + str(n.convert_yes_no(instance[3]))
 
     print_double_line('Naive Bayes Stratified k-Folds Predictive Accuracy')
 
     s = StratifiedFolds(table, indexes, label_index)
-    
+
     stratified_folds_matrix = s.stratified_k_folds(10)
 
     stratified_folds_accuracy = 1
@@ -88,9 +91,12 @@ def naive_bayes_titanic(table, indexes, label_index):  # step 1
     print '\tRandomSubsample(k=10, 2:1 Train / Test)'
     print '\t\taccuracy = ' + str(1) + ', error rate = ' + str(0)
     print '\tStratified 10-Fold Cross Validation'
-    print '\t\taccuracy = ' + str(stratified_folds_accuracy) + ', error rate = ' + str(1 - stratified_folds_accuracy)
+    print '\t\taccuracy = ' + str(stratified_folds_accuracy) + ', error rate = '\
+        + str(1 - stratified_folds_accuracy)
 
     print_double_line('Naive Bayes Confusion Matrix Predictive Accuracy')
+
+
 def cont_naive_bayes(table):  # step 2
 
     print_double_line('STEP 2a: Continuous Naive Bayes Classifier')
@@ -99,7 +105,8 @@ def cont_naive_bayes(table):  # step 2
     for instance in sample(table, 5):
         print '\tinstance: ' + str(instance)
         print '\tclass: ' + str(n.classify(instance)) \
-              + ' actual: ' + str(n.convert(instance[0], [13, 14, 16, 19, 23, 26, 30, 36, 44]))
+              + ' actual: ' + \
+            str(n.convert(instance[0], [13, 14, 16, 19, 23, 26, 30, 36, 44]))
 
     print_double_line('Step 2b: Predictive Accuracy')
 
@@ -111,7 +118,7 @@ def cont_naive_bayes(table):  # step 2
     print '\tRandomSubsample(k=10, 2:1 Train / Test)'
     print '\t\taccuracy = ' + str(1) + ', error rate = ' + str(0)
     print '\tStratified 10-Fold Cross Validation'
-    print '\t\taccuracy = ' + str(stratified_folds_accuracy) + ', error rate = ' + str(1-stratified_folds_accuracy)
+    print '\t\taccuracy = ' + str(stratified_folds_accuracy) + ', error rate = ' + str(1 - stratified_folds_accuracy)
 
     print_double_line('STEP 2c: Confusion Matrix')
     print_confusion(stratified_folds_matrix)
