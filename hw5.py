43442e7f4191adb8d764b17ff1349a972fa0ab0b
tdurnford <tjdford@gmail.com>
2016-11-03 10:56:57 -0700

    Major changes

--- hw5.py
+++ hw5.py
@@ -4,8 +4,8 @@ from copy import deepcopy
 from graphviz import Graph
 
 from hw1 import get_column, get_column_as_floats
-from hw3 import read_csv, remove_incomplete_rows, print_confusion, print_double_line
-from hw4 import StratifiedFolds, RandomSampling, StratifiedFoldsKnn, print_confusion_titanic
+from hw3 import read_csv, remove_incomplete_rows, print_confusion, print_double_line, COLUMN_NAMES
+from hw4 import StratifiedFolds, RandomSampling, StratifiedFoldsKnn, print_confusion_titanic, random_sampling
 
 
 class Discretization:
@@ -21,7 +21,9 @@ class Discretization:
         return table
 
     def categorize_instance(self, row):
-        pass
+
+        row[0] = str(self.convert(row[0], [13, 14, 16, 19, 23, 26, 30, 36, 44]))
+        row[4] = str(self.convert(row[4], [1999, 2499, 2999, 3499]))
 
     @staticmethod
     def convert(value, cutoffs):
@@ -44,7 +46,7 @@ class DisplayTree:
     # recursive helper function
     def save_graphviz_tree(self, file_name):
         self.dot = Graph()
-        # self.dot.graph_attr['rankdir'] = 'LR'
+        self.dot.graph_attr['rankdir'] = 'LR'
         self.create_dot_graph(self.decision_tree)
         self.dot.render(file_name)
 
@@ -52,13 +54,13 @@ class DisplayTree:
 
         if number == 0 and isinstance(tree, list):
             name = 'h' + str(number)
-            self.dot.node(name, 'Attribute: ' + str(self.attribute_names[tree[0]]), shape='box')
+            self.dot.node(name, 'Attribute: ' + str(self.attribute_names[int(tree[0])]), shape='box')
             number += 1
             self.create_dot_graph(tree[1], name, number)
 
         elif isinstance(tree, list):
-            name = head_name + str(self.attribute_names[tree[0]]) + str(number)
-            self.dot.node(name, 'Attribute: ' + str(self.attribute_names[tree[0]]), shape='box')
+            name = head_name + str(self.attribute_names[int(tree[0])]) + str(number)
+            self.dot.node(name, 'Attribute: ' + str(self.attribute_names[int(tree[0])]), shape='box')
             self.dot.edge(head_name, name)
             number += 1
             self.create_dot_graph(tree[1], name, number)
@@ -84,7 +86,7 @@ class DisplayTree:
     def print_ifs(self, tree, statement='if '):
 
         if isinstance(tree, list):
-            state = statement + str(self.attribute_names[tree[0]]) + ' == '
+            state = statement + str(self.attribute_names[int(tree[0])]) + ' == '
             self.print_ifs(tree[1], state)
 
         elif isinstance(tree, dict):
@@ -95,12 +97,11 @@ class DisplayTree:
             print statement[:-5] + ' then label is ' + tree
 
 
-class DecisionTree(Discretization, DisplayTree):
+class DecisionTree(DisplayTree, Discretization):
 
     def __init__(self, training_set, att_indexes, label_index):
         Discretization.__init__(self)
-
-        self.training_set = self.categorize_table(deepcopy(training_set))
+        self.training_set = deepcopy(training_set)
         self.training_set = training_set
         self.att_indexes = att_indexes
         self.label_index = label_index
@@ -108,9 +109,7 @@ class DecisionTree(Discretization, DisplayTree):
         self.att_domains = self.get_attribute_domains(self.training_set, self.att_indexes)
 
         self.decision_tree = self.tdidt(self.training_set, self.att_indexes)
-        DisplayTree.__init__(self, self.decision_tree, ['Class', 'Age', 'Sex', 'Survived'])
-        self.print_if_statements()
-        print self.decision_tree
+
 
     # [att_index_a, {value_a1: [att_index_b, {value_b1: yes, value_b2: no}], value_a2: no}]
     def tdidt(self, instances, att_indexes):
@@ -119,10 +118,13 @@ class DecisionTree(Discretization, DisplayTree):
 
             return instances[0][self.label_index]
 
-        elif len(att_indexes) == 1:
+        elif len(instances) == 0:
+
+            return 'go back you screwed up'
+
+        elif len(att_indexes) == 0:
 
             probabilities = self.get_probabilities(instances)
-            probabilities.sort(key=lambda x: x[1])
             return probabilities[0][0]
 
         else:
@@ -132,14 +134,26 @@ class DecisionTree(Discretization, DisplayTree):
             partitions = self.group_by(instances, part_index)
             indexes.remove(part_index)
 
-            return [part_index, {key: self.tdidt(partitions[key], indexes) for key in partitions}]
+            dictionary = {}
+
+            for key in partitions:
+
+                td = self.tdidt(partitions[key], indexes)
+
+                if td == 'go back you screwed up':
+                    probabilities = self.get_probabilities(instances)
+                    dictionary.update({key: probabilities[0][0]})
+                else:
+                    dictionary.update({key: td})
+
+            return [str(part_index), dictionary]
 
     def get_probabilities(self, instances):
         column = get_column(instances, self.label_index)
         labels = list(set(column))
 
         probabilities = [[label, self.count_if(column, label)] for label in labels]
-
+        probabilities.sort(key=lambda x: x[1], reverse=True)
         return probabilities
 
     @staticmethod
@@ -161,16 +175,13 @@ class DecisionTree(Discretization, DisplayTree):
 
         return self.att_domains
 
-    @staticmethod
-    def group_by(instances, index):
+    def group_by(self, instances, index):
 
-        dictionary = {}
+        dictionary = {att: [] for att in self.att_domains[index]}
+
         for row in instances:
-            if row[index] in dictionary:
-                dictionary[row[index]].append(row)
-            else:
-                dictionary.update({row[index]: []})
-                # print 'added key:  ' + str(row[index])
+            dictionary[row[index]].append(row)
+
         return dictionary
 
     # calculates enew using entropy stuff.
@@ -231,22 +242,36 @@ class DecisionTree(Discretization, DisplayTree):
     # uses the tree to predict the class label for the instance
     # returns the predicted label for the instance
     def classify(self, instance):
-        pass
-    #     return instances[self.label_index]
+        inst = instance[:]
+        self.categorize_instance(inst)
 
-        # else:
+        label = self.classify_recursive(self.decision_tree, inst)
+
+        if str(label) == 'yes':
+            return 0
+        elif str(label) == 'no':
+            return 1
+        else:
+            return label
+
+    # recursive helper function
+    def classify_recursive(self, tree, instance):
+
+        if isinstance(tree, list):
+            if str(instance[int(tree[0])]) not in tree[1]:
+                return 1
+            else:
+                return self.classify_recursive(tree[1][str(instance[int(tree[0])])], instance)
+        else:
+            return tree
 
 
-class AutoDecisionTree (DecisionTree):
+class AutoDecisionTree (Discretization, DecisionTree):
 
     def __init__(self, training_set, att_indexes, label_index):
-        DecisionTree.__init__(self, training_set, att_indexes, label_index)
-
-
-    def categorize_instance(self, row):
-
-        row[0] = self.convert(row[0], [13, 14, 16, 19, 23, 26, 30, 36, 44])
-        row[4] = str(self.convert(row[4], [1999, 2499, 2999, 3499]))
+        DecisionTree.__init__(self, self.categorize_table(deepcopy(training_set)), att_indexes, label_index)
+        DisplayTree.__init__(self, self.decision_tree, COLUMN_NAMES)
+        self.save_graphviz_tree('auto-data')
 
 
 class AutoStratifiedFolds(StratifiedFolds):
@@ -259,6 +284,7 @@ class AutoStratifiedFolds(StratifiedFolds):
 
     def categorize_instance(self, row):
         pass
+
 
 class AutoRandomSampling(RandomSampling):
 
@@ -273,9 +299,10 @@ class TitanicDecisionTree(DecisionTree):
 
     def __init__(self, training_set, att_indexes, label_index):
         DecisionTree.__init__(self, training_set, att_indexes, label_index)
+        DisplayTree.__init__(self, self.decision_tree, ['Class', 'Age', 'Sex', 'Survived'])
 
-    def classify(self, instance):
-        return 0
+    def categorize_instance(self, row):
+        return None
 
 
 class TitanicStratifiedFolds(StratifiedFoldsKnn):
@@ -293,30 +320,42 @@ def titanic_decision_tree(table, indexes, label_index):  # step 1
 
     d = TitanicDecisionTree(table, indexes, label_index)
 
-    # for instance in sample(table, 5):
-    #     print '\tinstance: ' + str(instance)
-    #     print '\tclass: ' + str(d.classify(instance)) + ' actual: '\
-    #         + str(instance[3])
+    for instance in sample(table, 5):
+        print '\tinstance: ' + str(instance)
+        print '\tclass: ' + str(convert(d.classify(instance))) + ' actual: '\
+            + str(instance[3])
 
-    # print_double_line('Titanic Decision Tree k-Folds Predictive Accuracy')
+    print_double_line('Titanic Decision Tree k-Folds Predictive Accuracy')
 
-    # s = TitanicStratifiedFolds(table, indexes, label_index)
+    s = TitanicStratifiedFolds(table, indexes, label_index)
 
-    # stratified_folds_matrix = s.stratified_k_folds(10)
-    # # random_sampling = TitanicRandomSampling(table, [1, 4, 6], 0, 10)
+    stratified_folds_matrix = s.stratified_k_folds(10)
 
-    # stratified_folds_accuracy = s.get_accuracy_of_confusion(stratified_folds_matrix)[0]
-    # # random_sampling_accuracy = random_sampling.random_sampling()
+    stratified_folds_accuracy = s.get_accuracy_of_confusion(stratified_folds_matrix)[0]
+    random_sampling_accuracy = random_sampling
 
-    # print '\tRandomSubsample(k=10, 2:1 Train / Test)'
-    # print '\t\taccuracy = ' + str(0.71) + ', error rate = ' + str(1-0.71)
-    # print '\tStratified 10-Fold Cross Validation'
-    # print '\t\taccuracy = ' + str(stratified_folds_accuracy) + ', error rate = '\
-    #     + str(1 - stratified_folds_accuracy)
+    print '\tRandomSubsample(k=10, 2:1 Train / Test)'
+    print '\t\taccuracy = ' + str(random_sampling_accuracy) + ', error rate = ' + str(1-random_sampling_accuracy)
+    print '\tStratified 10-Fold Cross Validation'
+    print '\t\taccuracy = ' + str(stratified_folds_accuracy) + ', error rate = '\
+        + str(1 - stratified_folds_accuracy)
 
-    # print_double_line('Titanic Decision Tree Confusion Matrix Predictive Accuracy')
+    print_double_line('Titanic Decision Tree Confusion Matrix Predictive Accuracy')
 
-    # print_confusion_titanic(stratified_folds_matrix)
+    print_confusion_titanic(stratified_folds_matrix)
+
+    print_double_line('Titanic If Statements')
+
+    d.print_if_statements()
+
+    d.save_graphviz_tree('titanic_decision_tree')
+
+
+def convert(value):
+    if value == 0:
+        return 'no'
+    else:
+        return 'yes'
 
 
 def auto_decision_tree(table, indexes, label_index):  # step 2
@@ -324,37 +363,35 @@ def auto_decision_tree(table, indexes, label_index):  # step 2
     print_double_line('Auto Decision Tree Classifier')
     d = AutoDecisionTree(table, indexes, label_index)
 
-    # for instance in sample(table, 5):
-    #     print '\tinstance: ' + str(instance)
-    #     print '\tclass: ' + str(d.classify(instance)) + ' actual: '\
-    #         + str(d.convert(instance[0], [13, 14, 16, 19, 23, 26, 30, 36, 44]))
-
-    # print_double_line('Decision Tree k-Folds Predictive Accuracy')
-    # d.classify(table, label_index)
-    # s = AutoStratifiedFolds(table, indexes, label_index)
+    for instance in sample(table, 5):
+        print '\tinstance: ' + str(instance)
+        print '\tclass: ' + str(d.classify(instance)) + ' actual: '\
+            + str(d.convert(instance[0], [13, 14, 16, 19, 23, 26, 30, 36, 44]))
 
     print_double_line('Auto Decision Tree k-Folds Predictive Accuracy')
 
-    # stratified_folds_matrix = s.stratified_k_folds(10)
-    # random_sampling = AutoRandomSampling(table, [1, 4, 6], 0, 10)
+    s = AutoStratifiedFolds(table, indexes, label_index)
 
-    # stratified_folds_accuracy = s.get_accuracy_of_confusion(
-    #     stratified_folds_matrix)[0]
-    # random_sampling_accuracy = random_sampling.random_sampling()
+    stratified_folds_matrix = s.stratified_k_folds(10)
+    random_sampling = AutoRandomSampling(table, [1, 4, 6], 0, 10)
 
-    # print '\tRandomSubsample(k=10, 2:1 Train / Test)'
-    # print '\t\taccuracy = ' + str(random_sampling_accuracy) + ', error rate = ' + str(0)
-    # print '\tStratified 10-Fold Cross Validation'
-    # print '\t\taccuracy = ' + str(stratified_folds_accuracy) + ', error rate = '\
-    #     + str(1 - stratified_folds_accuracy)
+    stratified_folds_accuracy = s.get_accuracy_of_confusion(stratified_folds_matrix)[0]
+    random_sampling_accuracy = random_sampling.random_sampling()
 
-    # print '\tRandomSubsample(k=10, 2:1 Train / Test)'
-    # print '\t\taccuracy = ' + str(random_sampling_accuracy) + ', error rate = ' + str(1-random_sampling_accuracy)
-    # print '\tStratified 10-Fold Cross Validation'
-    # print '\t\taccuracy = ' + str(stratified_folds_accuracy) + ', error rate = '\
-    #     + str(1 - stratified_folds_accuracy)
+    print '\tRandomSubsample(k=10, 2:1 Train / Test)'
+    print '\t\taccuracy = ' + str(random_sampling_accuracy) + ', error rate = ' + str(1 - random_sampling_accuracy)
+    print '\tStratified 10-Fold Cross Validation'
+    print '\t\taccuracy = ' + str(stratified_folds_accuracy) + ', error rate = ' + str(1 - stratified_folds_accuracy)
 
-    # print_double_line('Auto Decision Tree Confusion Matrix Predictive Accuracy')
+    print_double_line('Auto Decision Tree Confusion Matrix Predictive Accuracy')
+
+    print_confusion(stratified_folds_matrix)
+
+    print_double_line('Auto Decision If Statements')
+
+    d.print_if_statements()
+
+    d.save_graphviz_tree('auto_data_decision_tree')
 
 
 def main():
@@ -362,7 +399,8 @@ def main():
     table = remove_incomplete_rows(read_csv('auto-data.txt'))
     table_titanic = remove_incomplete_rows(read_csv('titanic.txt')[1:])
 
-    # auto_decision_tree(table, [1, 4, 6], 0)
-    titanic_decision_tree(table_titanic, [0, 1, 2], 3)
+    titanic_decision_tree(table_titanic, [0, 1,  2], 3)
+    auto_decision_tree(table, [1, 4, 6], 0)
 
-main()
+if __name__ == '__main__':
+    main()
