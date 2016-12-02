c3265f3df49e32fa2f9a0c1338461dcd04752b35
tdurnford <tjdford@gmail.com>
2016-12-02 11:46:56 -0800

    playing around

--- hw7.py
+++ hw7.py
@@ -1,4 +1,5 @@
-from hw6 import remove_incomplete_rows, read_csv
+from hw6 import remove_incomplete_rows, read_csv, get_column
+
 
 class Apriori:
 
@@ -9,6 +10,13 @@ class Apriori:
         self.min_support = min_support
         self.min_confidence = min_confidence
 
+        self.att_domains = [set(get_column(self.table, att)) for att in range(len(self.table[0]))]
+        self.all = [{item} for lists in self.att_domains for item in lists if self.get_support([item]) >= self.min_support]
+        self.all_sets = []
+
+        print self.all
+       
+
     # returns true if all items in items are contained in item_set; otherwise, false.
     def contains(self, items, item_set):
         if len(items) == 1:
@@ -17,7 +25,6 @@ class Apriori:
             return items[0] in item_set and self.contains(items[1:], item_set)
 
     def get_support(self, items):
-
         return len([row for row in self.table if self.contains(items, row)])/self.length
 
     def get_confidence(self, left, items):
@@ -26,8 +33,10 @@ class Apriori:
 
 
 def main():
+
     table = remove_incomplete_rows(read_csv('titanic.txt')[1:])
 
-    a = Apriori(table, 0.25, 0.80)
+    a = Apriori(table, 0.1, 0.80)
+
 
 main()
